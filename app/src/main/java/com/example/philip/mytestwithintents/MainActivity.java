fdfb9ffd7c417e3130c6a4a3b0286d51d670bd35
Philip G <pga4711@gmail.com>
2018-03-27 09:06:51 +0200

    The OEM Native Camera works. Gallerybuttons removed.

--- app/src/main/java/com/example/philip/mytestwithintents/MainActivity.java
+++ app/src/main/java/com/example/philip/mytestwithintents/MainActivity.java
@@ -2,25 +2,21 @@ package com.example.philip.mytestwithintents;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.database.Cursor; //??okej?
-import android.content.Context; //????????????
 import android.net.Uri; //?????
 import android.widget.ImageView;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -30,13 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = AppCompatActivity.class.getName();
     public String lastCameraFileUri;
-
-    static final int REQUEST_OPEN_IMAGE = 1;
-
-    static final int REQUEST_OPEN_CAMERA = 2;
-    static final int REQUEST_PERMISSIONS = 3;
-
-    static boolean ALL_PERMISSIONS_OK = false;
+    static final int REQUEST_OPEN_CAMERA =1;
+    static final int REQUEST_PERMISSIONS = 2;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,51 +39,40 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "SDK version is: " + Build.VERSION.SDK_INT);
         Log.d(TAG, "Applicationid  is: " + BuildConfig.APPLICATION_ID);
 
-        //THIS IS ONLY ALLOWED IN PROTOTYPE SOFTWARE, in a real realease on Google Play you have to handle
+        //THIS IS ONLY ALLOWED IN PROTOTYPE SOFTWARE, in a real release on Google Play you have to handle
         //permissions if the user presses DENY.
         String[] permissions = {Manifest.permission.CAMERA,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.RECORD_AUDIO};
 
-
         ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSIONS);
-
-
-
-        final Button callGalleryBtn = findViewById(R.id.buttonX);
-        callGalleryBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                Log.d(TAG, "This is intent.getType(): " + intent.getType());
-                intent.setType("image/*");
-                Log.d(TAG, "This is intent.getType(): " + intent.getType());
-                Log.d(TAG, "this.getClass() : " + this.getClass());
-                startActivityForResult(intent, REQUEST_OPEN_IMAGE);
-            }
-        });
-
 
         final Button callCameraBtn = findViewById(R.id.callCameraButton);
         callCameraBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 dispatchTakePictureIntent();
 
             }
         });
+
+
+        handler = new Handler();
+
+        final Runnable r = new Runnable() {
+            public void run() {
+                Log.d(TAG, "HELLO FROM DELAY-THING");
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.postDelayed(r, 1000);
     }
     //Denna anropas alltid när en activity är typ klar. Den funkar osm ett slot i QT.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == REQUEST_OPEN_IMAGE) {
-            Log.d(TAG, "HELLO, This is data: " + data.getDataString());
-            //Lite avancerad
-            String filePath = getRealPathFromURI(getApplicationContext(), data.getData());
-            Log.d(TAG, "WHat could this filePath be? " + filePath);
-
-        } else if (requestCode == REQUEST_OPEN_CAMERA) {
+        if  (requestCode == REQUEST_OPEN_CAMERA) {
             Log.d(TAG,"We arrived to read the file");
             Log.d(TAG,"This should be the lastCameraFileUri: "+ lastCameraFileUri);
             //och vad gör männskann här. När hen har fått tillbaka en bild. Jo
@@ -99,23 +80,6 @@ public class MainActivity extends AppCompatActivity {
         } else if (requestCode == REQUEST_PERMISSIONS) {
             Log.d(TAG, "permission jox har körts");
         }
-    }
-//Anropas endast vid request image! inte vid kameraanrop
-
-    public String getRealPathFromURI(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-        //Frivilligt:  super.onActivityResult(requestCode, resultCode, data);
     }
 
     //Call Camera funktionalities
@@ -130,9 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
             File photoFile = null;
             try {
-
                 photoFile = createImageFile();
-
             } catch (IOException ex) {
                 // Error occurred while creating the File
                 Log.d(TAG, "ERROR ex.getLocalizedMessage() : " + ex.getLocalizedMessage());
@@ -150,13 +112,6 @@ public class MainActivity extends AppCompatActivity {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_OPEN_CAMERA);
 
-
-                //Gammalt sätt
-                /*
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                lastCameraFileUri = photoFile.toString();
-                startActivityForResult(takePictureIntent, REQUEST_OPEN_CAMERA);
-                */
             }
         } else {
             Log.d(TAG, "ERROR. Problems with your camera?!");
@@ -167,26 +122,20 @@ public class MainActivity extends AppCompatActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 
-
         String imageFileName = "XBalancer" + timeStamp;
-
-
         //Okej, här får vi platsen där alla bilder sparas
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
         Log.d(TAG, "This is storageDir.toString() : " + storageDir.toString());
 
-
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         Log.d(TAG, "This is image.toString()      : " + image.toString());
-
 
         return image;
     }
 
-
     private void setPic() {
-
+        Log.d(TAG, "This is lastCameraFileUri: "+lastCameraFileUri);
         ImageView mImageView = findViewById(R.id.imageView1);
         // Get the dimensions of the View
         int targetW = mImageView.getWidth();
@@ -210,5 +159,4 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeFile(lastCameraFileUri, bmOptions);
         mImageView.setImageBitmap(bitmap);
     }
-
 }
